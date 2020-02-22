cb947fa7908c057c6b49e35b90c32c7d886995aa
prmawyer <60946845+prmawyer@users.noreply.github.com>
2020-02-22 17:34:50 -0500

    rename

--- exercise_2/variables.tf
+++ exercise_2/variables.tf
@@ -16,7 +16,7 @@ variable "s3_bucket_region" {
 variable "s3_bucket_name" {
   type        = "string"
   description = "Name of the S3 bucket to be created used to store the remote state file"
-  default     = "pmawyer-harness-terraform-exercise2"
+  default     = "pmawyer-harness-terraform-exercise2-rename"
   }
 
 variable "tag_env" {
